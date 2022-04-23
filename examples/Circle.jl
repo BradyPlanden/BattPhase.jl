23aa430aa4d47d853161d2c901d3ba9fc0584805
Brady Planden <info@bradyplanden.com>
2022-04-23 10:22:59 +0100

    Profiler / Package Dependencies / Example Updt.

--- examples/Circle.jl
+++ examples/Circle.jl
@@ -1,26 +1,23 @@
-using BattPhase, Plots, BenchmarkTools, SparseArrays, ProfileView
-
-@inline function Circle()
+using BattPhase, Plots, BenchmarkTools, SparseArrays, ProfileView, Infiltrator, DifferentialEquations, MKL
 
     ## Discretisation Parameters
     Nx = NN = MM = Ny = Int64(160)
     δ = 0.1
     tf = 2.
-    ν = 1.
-    ki₀ = 1.
+    ν = ki₀ = 1.
 
     N = Int64(NN+2)
     h = 1/NN
-    M = MM+2
+    M = Int64(MM+2)
     Ntot = Int64(N*M)
 
 
     Y₀ = Array{Float64}(undef,N,M)
     eq = Array{Float64}(undef,N,M)
     F₀ = Array{Float64}(undef,N,M)
-    Φ₀ = Vector{Float64}(undef,Ntot) .=0
+    Φ₀ = Vector{Float64}(undef,Ntot) .= 0
     #Φ₀ = sparsevec(Φ₀)
-    ff = copy(Φ₀)
+    ff = Vector{Float64}(undef,Ntot) .= 0
     #j₀ = Array{Float64}(undef,Ntot,Ntot) #Modify to be Sparse
     j₀ = spzeros(Ntot,Ntot)
     ymid = Array{Float64}(undef,N,M)
@@ -36,21 +33,25 @@ using BattPhase, Plots, BenchmarkTools, SparseArrays, ProfileView
     #kk = @benchmark Eqs11!($Y₀,$Φ₀,$δ,$ki₀,$ff,$N,$M)
     #ttq = @benchmark Jac!($Y₀,$Φ₀,$δ,$ki₀,$j₀,$N,$M)
     y₀!(NN,MM,Y₀)
-    Eqs11!(Y₀,Φ₀,δ,ki₀,ff,N,M)
-    Jac!(Y₀,Φ₀,δ,ki₀,j₀,N,M)
+    Eqs11!(Y₀,Φ₀,δ,ki₀,ff,N,M,h)
+    Jac!(Y₀,Φ₀,δ,ki₀,j₀,N,M,h)
 
     dᵦ = j₀\-ff
     Φ₀ += dᵦ
 
+
     vv₀ = max(abs(Φ₀[Ntot-2*N+1]),abs(Φ₀[Ntot-2*N+N÷2]),abs(Φ₀[Ntot-2*N+N÷2+1]),abs(Φ₀[Ntot-N]))
+
+    #@infiltrate cond=true
+
     tt = 0.
     dt = min(h/vv₀/ν/ki₀,tf-tt)
-    Nₜ = round(Int,tf/dt)+50
+    Nₜ = round(Int64,tf/dt)+50
     V = Vector{Float64}(undef,Nₜ)
-    TT = Vector{Float64}(undef,Nₜ)
+    TT = Vector{Float64}(undef,Nₜ) .= 0
     Φₐ = Vector{Float64}(undef,Nₜ)
     V[1] = (Φ₀[Int(Ntot-2*N+N/2)]/2+Φ₀[Int(Ntot-2*N+N/2+1)]/2)+h/2*δ
-    TT[1] = 0.
+    #TT[1] = 0.
 
 
     Φₐ[1] = Φᵥ₊(Y₀,N,M,Ny)
@@ -63,30 +64,30 @@ using BattPhase, Plots, BenchmarkTools, SparseArrays, ProfileView
     #fgv = @benchmark $j₀\-$ff
     ii = 1
 
-    function solve!(Y₀,Φ₀,F₀,dt,N,M,δ,ki₀,ymid,ii,j₀,Ntot,tt,tf)
+    @inline function solve!(Y₀,Φ₀,F₀,dt,N,M,δ,ki₀::Float64,ymid,ii,j₀,Ntot,tt::Float64,tf::Float64,Φₐ,TT,V,Nₜ,ff,dᵦ,ν::Float64,vv₀::Float64,h::Float64)
         while tt < tf
 
             #Ef!
-            Upwind!(Y₀,Φ₀,F₀,dt,N,M,δ)
+            Upwind!(Y₀,Φ₀,F₀,dt,N,M,δ,h)
             Ef!(Y₀,ymid,Φ₀,F₀,dt,ν,ki₀,N,M)
-            Eqs11!(ymid,Φ₀,δ,ki₀,ff,N,M)
-            Jac!(ymid,Φ₀,δ,ki₀,j₀,N,M)
+            Eqs11!(ymid,Φ₀,δ,ki₀,ff,N,M,h)
+            Jac!(ymid,Φ₀,δ,ki₀,j₀,N,M,h)
             dᵦ = j₀\-ff
             Φ₊!(Ntot,Φ₀,dᵦ)
 
             #Ef2!
-            Upwind!(ymid,Φ₀,F₀,dt,N,M,δ)
+            Upwind!(ymid,Φ₀,F₀,dt,N,M,δ,h)
             Ef2!(Y₀,ymid,Φ₀,F₀,dt,ν,ki₀,N,M)
-            Eqs11!(ymid,Φ₀,δ,ki₀,ff,N,M)
-            Jac!(ymid,Φ₀,δ,ki₀,j₀,N,M)
+            Eqs11!(ymid,Φ₀,δ,ki₀,ff,N,M,h)
+            Jac!(ymid,Φ₀,δ,ki₀,j₀,N,M,h)
             dᵦ = j₀\-ff
             Φ₊!(Ntot,Φ₀,dᵦ)
 
             #Ef3!
-            Upwind!(ymid,Φ₀,F₀,dt,N,M,δ)
+            Upwind!(ymid,Φ₀,F₀,dt,N,M,δ,h)
             Ef3!(Y₀,ymid,Φ₀,F₀,dt,ν,ki₀,N,M)
-            Eqs11!(Y₀,Φ₀,δ,ki₀,ff,N,M)
-            Jac!(Y₀,Φ₀,δ,ki₀,j₀,N,M)
+            Eqs11!(Y₀,Φ₀,δ,ki₀,ff,N,M,h)
+            Jac!(Y₀,Φ₀,δ,ki₀,j₀,N,M,h)
             dᵦ = j₀\-ff
             Φ₊!(Ntot,Φ₀,dᵦ)
 
@@ -94,7 +95,7 @@ using BattPhase, Plots, BenchmarkTools, SparseArrays, ProfileView
             @show ii += 1
             V[ii] = (Φ₀[Ntot-2*N+N÷2]/2+Φ₀[Ntot-2*N+N÷2+1]/2)+h/2*δ
             TT[ii] = TT[ii-1]+dt
-            tt = tt+dt
+            tt += dt
             YStore(Y₀,Ydata,N,M,ii+1)
             Φₐ[ii] = Φᵥ₊(Y₀,N,M,Ny)
             vv₀ = max(abs(Φ₀[Ntot-2*N+1]),abs(Φ₀[Ntot-2*N+N÷2]),abs(Φ₀[Ntot-2*N+N÷2+1]),abs(Φ₀[Ntot-N]))
@@ -103,17 +104,16 @@ using BattPhase, Plots, BenchmarkTools, SparseArrays, ProfileView
             #Φₐ[i]:=Φᵥ₊(Y₀,N,M,Ny);
 
         end
-       
+        return Ydata, V, TT
     end
 
-    @time solve!(Y₀,Φ₀,F₀,dt,N,M,δ,ki₀,ymid,ii,j₀,Ntot,tt,tf)
+    #@time solve!(Y₀,Φ₀,F₀,dt,N,M,δ,ki₀,ymid,ii,j₀,Ntot,tt,tf,Φₐ,TT,V,Nₜ)
 
-    return Ydata, V, TT
     #return kk, ttq, qqw, rrt, fgv
 
-end
 
-Ydata, V, TT = Circle()
+@time solve!(Y₀,Φ₀,F₀,dt,N,M,δ,ki₀,ymid,ii,j₀,Ntot,tt,tf,Φₐ,TT,V,Nₜ,ff,dᵦ,ν,vv₀,h)
 #kk,ttq, qqw, rrt, fgv = Circle()
-#@profview Circle()
-#@code_warntype Circle()
+#@profview solve!(Y₀,Φ₀,F₀,dt,N,M,δ,ki₀,ymid,ii,j₀,Ntot,tt,tf,Φₐ,TT,V,Nₜ,ff,dᵦ,ν,vv₀,h)
+#@code_warntype solve!(Y₀,Φ₀,F₀,dt,N,M,δ,ki₀,ymid,ii,j₀,Ntot,tt,tf,Φₐ,TT,V,Nₜ,ff,dᵦ,ν,vv₀,h)
+#Ydata, V, TT = solve!(Y₀,Φ₀,F₀,dt,N,M,δ,ki₀,ymid,ii,j₀,Ntot,tt,tf,Φₐ,TT,V,Nₜ,ff,dᵦ,ν,vv₀,h)
--- src/custom_functions.jl
+++ src/custom_functions.jl
@@ -4,9 +4,9 @@ function y₀!(NN,MM,Y₀)
     h = 1.0/NN
     w = h/2
     M = Ny = MM+2
-    for i ∈ 2:N-1 for j ∈ 2:M-1
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1
         rr = ((i-3/2)*h)^2+((j-3/2)*h)^2 #combining two variables
-        Y₀[i,j] = max(1e-9,0.5*tanh(sqrt(2)*(sqrt(rr)-0.3)/w)+0.5) #eps() replaces 1e-9
+        Y₀[i,j] = max(eps(),0.5*tanh(sqrt(2)*(sqrt(rr)-0.3)/w)+0.5) #eps() replaces 1e-9
 
         for i ∈ 1:N 
             Y₀[i,1] = Y₀[i,2]
@@ -20,46 +20,44 @@ function y₀!(NN,MM,Y₀)
 end  
 
 ## System Eqs
-function Eqs11!(Y₀,y,δ,ki₀,ff,N,M)
-    h=1.0/(N-2)
+function Eqs11!(Y₀,y,δ,ki₀,ff,N,M,h)
 
-    for i ∈ 2:N-1 for j ∈ 2:M-1
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1 
         i1 = i+(j-1)*N
-        ff[i1] = (Y₀[i,j]+Y₀[i,j+1])*(y[i1+N]-y[i1]) -
+        @views ff[i1] = (Y₀[i,j]+Y₀[i,j+1])*(y[i1+N]-y[i1]) -
         (Y₀[i,j]+Y₀[i,j-1])*(y[i1]-y[i1-N]) +
         (Y₀[i+1,j]+Y₀[i,j])*(y[i1+1]-y[i1]) -
         (Y₀[i,j]+Y₀[i-1,j])*(y[i1]-y[i1-1]) -
         ki₀*y[i1]*h*(1e-24+(Y₀[i+1,j]-Y₀[i-1,j])^2+(Y₀[i,j+1]-Y₀[i,j-1])^2)^(1/2)
     end end
 
-    for i ∈ 1:1 for j ∈ 1:M
+    for j ∈ 1:M @simd for i ∈ 1:1 
         i1 = i+(j-1)*N
-        ff[i1] = -y[i1]+y[i1+1]
+        @views ff[i1] = -y[i1]+y[i1+1]
     end end
 
-    for i ∈ N:N for j ∈ 1:M
+    for j ∈ 1:M @simd for i ∈ N:N 
         i1 = i+(j-1)*N
-        ff[i1] = -y[i1]+y[i1-1]
+        @views ff[i1] = -y[i1]+y[i1-1]
     end end
 
-    for i ∈ 1:N for j ∈ 1:1
+    for j ∈ 1:1 @simd for i ∈ 1:N 
         i1 = i+(j-1)*N
-        ff[i1] = -y[i1]+y[i1+N]
+        @views ff[i1] = -y[i1]+y[i1+N]
     end end
 
-    for i ∈ 1:N for j ∈ M:M
+    for j ∈ M:M @simd for i ∈ 1:N 
         i1 = i+(j-1)*N
-        ff[i1] = -y[i1]+y[i1-N]+δ*h
+        @views ff[i1] = -y[i1]+y[i1-N]+δ*h
     end end
 end
 
 ## Jacobian
-function Jac!(Y₀,y,δ,ki₀,j₀,N,M)
-    h = 1/(N-2)
+function Jac!(Y₀,y,δ,ki₀,j₀,N,M,h)
 
-    for i ∈ 2:N-1 for j ∈ 2:M-1
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1 
         i1 = i+(j-1)*N
-        j₀[i1,i1] = -4*Y₀[i,j]-Y₀[i,j+1]-Y₀[i,j-1]-Y₀[i+1,j]-Y₀[i-1,j]-
+        @views j₀[i1,i1] = -4*Y₀[i,j]-Y₀[i,j+1]-Y₀[i,j-1]-Y₀[i+1,j]-Y₀[i-1,j]-
         ki₀*h*(1e-24+(Y₀[i+1,j]-Y₀[i-1,j])^2+(Y₀[i,j+1]-Y₀[i,j-1])^2)^(1/2)
         j₀[i1,i1+1] = Y₀[i+1,j]+Y₀[i,j]
         j₀[i1,i1-1] = Y₀[i-1,j]+Y₀[i,j]
@@ -67,57 +65,56 @@ function Jac!(Y₀,y,δ,ki₀,j₀,N,M)
         j₀[i1,i1-N] = Y₀[i,j-1]+Y₀[i,j]
     end end
 
-    for i ∈ 1:1 for j ∈ 1:M
+    for j ∈ 1:M @simd for i ∈ 1:1 
         i1 = i+(j-1)*N
-        j₀[i1,i1] = -1.
-        j₀[i1,i1+1] =1.
+        @views j₀[i1,i1] = -1.
+        @views j₀[i1,i1+1] =1.
         #ff[i1] = -y[i1]+y[i1+1]
     end end
 
-    for i ∈ N:N for j ∈ 1:M
+    for j ∈ 1:M @simd for i ∈ N:N 
         i1 = i+(j-1)*N
-        j₀[i1,i1] = -1.
-        j₀[i1,i1-1] = 1.
+        @views j₀[i1,i1] = -1.
+        @views j₀[i1,i1-1] = 1.
         #ff[i1]:=-y[i1]+y[i1-1]:
     end end
 
-    for i ∈ 1:N for j ∈ 1:1
+    for j ∈ 1:1 @simd for i ∈ 1:N 
         i1 = i+(j-1)*N
         #ff[i1] = -y[i1]+y[i1+N]
-        j₀[i1,i1] = -1.
-        j₀[i1,i1+N] =1.
+        @views j₀[i1,i1] = -1.
+        @views j₀[i1,i1+N] =1.
     end end
 
-    for i ∈ 1:N for j ∈ M:M
+    for j ∈ M:M @simd for i ∈ 1:N 
         i1 = i+(j-1)*N
-        j₀[i1,i1] = -1.
-        j₀[i1,i1-N] = 1.
+        @views j₀[i1,i1] = -1.
+        @views j₀[i1,i1-N] = 1.
         #ff[i1] = -y[i1]+y[i1-N]+δ*h
     end end 
 end
 
 
 ## Upwind
-function Upwind!(Y₀,Φ₀,F₀,dt,N,M,v₀)
-    h=1/(N-2)
+function Upwind!(Y₀,Φ₀,F₀,dt,N,M,v₀,h)
 
     vv₀ = v₀
-    for i ∈ 1:N
+    @views for i ∈ 1:N
         Y₀[i,1] = Y₀[i,2]
         Y₀[i,M] = Y₀[i,M-1]
     end
 
-    for j ∈ 1:M
+    @views for j ∈ 1:M
         Y₀[1,j] = Y₀[2,j]
         Y₀[N,j] = Y₀[N-1,j]
     end
 
-    for i ∈ 2:N-1 for j ∈ 2:M-1   
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1 
         vx = vy =0.
-        vx1 = (Y₀[i,j]-Y₀[i-1,j])/h
-        vx2 = (Y₀[i+1,j]-Y₀[i,j])/h
-        vy1 = (Y₀[i,j]-Y₀[i,j-1])/h
-        vy2 = (Y₀[i,j+1]-Y₀[i,j])/h
+        @views vx1 = (Y₀[i,j]-Y₀[i-1,j])/h
+        @views vx2 = (Y₀[i+1,j]-Y₀[i,j])/h
+        @views vy1 = (Y₀[i,j]-Y₀[i,j-1])/h
+        @views vy2 = (Y₀[i,j+1]-Y₀[i,j])/h
 
     if v₀ >= 0.
         vx1 = max(vx1,0) 
@@ -143,75 +140,75 @@ end
 
 # Φ+
 function Φ₊!(N,Φ₀,dᵦ)
-    for i ∈ 1:N 
+    @views for i ∈ 1:N 
         Φ₀[i] += dᵦ[i]
     end 
 end
 
 
 function Φᵥ₊(Y₀,N,M,Ny)
-    Φᵥ = 0.
-    for i ∈ 2:N-1 for j ∈ 2:M-1 
-        Φᵥ += Y₀[i,j]
+    Φᵥ= 0.
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1
+        @views Φᵥ += Y₀[i,j]
     end end
-    return Φᵥ/(M-2)/(N-2) # Need to confirm
+    return Φᵥ/(M-2)/(N-2)
 end
 
 
 ## EF Add 
 function Ef!(Y₀,ymid,Φ₀,F₀,dt,ν,ki₀,N,M)
     #for i from 2 to N-1 do for j from 2 to M-1 do ymid[i,j]:=Y₀[i,j]-dt*F₀[i,j]*Φ₀[i+(j-1)*N]:od:od:
-    for i ∈ 2:N-1 for j ∈ 2:M-1 
-        ymid[i,j] = max(1e-9,Y₀[i,j]-dt*ν*ki₀*F₀[i,j]*Φ₀[i+(j-1)*N])
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1  
+        @inbounds @views ymid[i,j] = max(eps(),Y₀[i,j]-dt*ν*ki₀*F₀[i,j]*Φ₀[i+(j-1)*N]) #1e-9
     end end
 
     for i ∈ 1:N
-        ymid[i,1] = ymid[i,2]
-        ymid[i,M] = ymid[i,M-1]
+        @inbounds @views ymid[i,1] = ymid[i,2]
+        @inbounds @views ymid[i,M] = ymid[i,M-1]
     end
 
     for j ∈ 1:M
-        ymid[1,j] = ymid[2,j]
-        ymid[N,j] = ymid[N-1,j]
+        @inbounds @views ymid[1,j] = ymid[2,j]
+        @inbounds @views ymid[N,j] = ymid[N-1,j]
     end
 end
 
 
 function Ef2!(Y₀,ymid,Φ₀,F₀,dt,ν,ki₀,N,M)
-    for i ∈ 2:N-1 for j ∈ 2:M-1
-        ymid[i,j] = max(1e-9,Y₀[i,j]*3/4+ymid[i,j]/4-dt/4*ν*ki₀*F₀[i,j]*Φ₀[i+(j-1)*N])
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1 
+        @inbounds @views ymid[i,j] = max(eps(),Y₀[i,j]*3/4+ymid[i,j]/4-dt/4*ν*ki₀*F₀[i,j]*Φ₀[i+(j-1)*N]) #1e-9
     end end
 
-    for i ∈ 1:N
-        ymid[i,1] = ymid[i,2]
-        ymid[i,M] = ymid[i,M-1]
+    @inbounds @views for i ∈ 1:N
+         ymid[i,1] = ymid[i,2]
+         ymid[i,M] = ymid[i,M-1]
     end
 
-    for j ∈ 1:M
-        ymid[1,j] = ymid[2,j]
-        ymid[N,j] = ymid[N-1,j]
+    @inbounds @views for j ∈ 1:M
+         ymid[1,j] = ymid[2,j]
+         ymid[N,j] = ymid[N-1,j]
     end
 end
 
 
 function Ef3!(Y₀,ymid,Φ₀,F₀,dt,ν,ki₀,N,M)
-    for i ∈ 2:N-1 for j ∈ 2:M-1
-        Y₀[i,j] = max(1e-9,Y₀[i,j]*1/3+ymid[i,j]*2/3-dt*2/3*ν*ki₀*F₀[i,j]*Φ₀[i+(j-1)*N])
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1 
+        @inbounds @views Y₀[i,j] = max(eps(),Y₀[i,j]*1/3+ymid[i,j]*2/3-dt*2/3*ν*ki₀*F₀[i,j]*Φ₀[i+(j-1)*N]) #1e-9
     end end
 
-        for i ∈ 1:N
-            Y₀[i,1] = Y₀[i,2]
-            Y₀[i,M] = Y₀[i,M-1]
+        @inbounds @views for i ∈ 1:N
+             Y₀[i,1] = Y₀[i,2]
+             Y₀[i,M] = Y₀[i,M-1]
         end
 
-        for j ∈ 1:M
+        @inbounds @views for j ∈ 1:M
             Y₀[1,j] = Y₀[2,j]
             Y₀[N,j] = Y₀[N-1,j]
         end
 end
 
 function YStore(Y₀,Ydata,N,M,jj)
-    for i ∈ 2:N-1 for j ∈ 2:M-1
-        Ydata[jj,i,j] = Y₀[i,j]
+    for j ∈ 2:M-1 @simd for i ∈ 2:N-1 
+        @inbounds @views Ydata[jj,i,j] = Y₀[i,j]
     end end
 end
