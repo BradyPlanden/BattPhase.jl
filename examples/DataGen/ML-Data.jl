a5b86a317e38c27faf7432b94e33caff01efb110
Brady Planden <info@bradyplanden.com>
2022-05-23 09:13:59 +0100

    Updt. ML-Data.jl for 2D data generation / valid+train+test

--- examples/DataGen/ML-Data.jl
+++ examples/DataGen/ML-Data.jl
@@ -4,35 +4,55 @@ function Data()
 
     PhaseFieldData = κ = tuple()
     δ = 0.1
-    tt = 0.
+    ti = 0.
     tf = 1.
     StepNum = 30
-    Δₜ = (tf-tt)/StepNum
+    Δₜ = (tf-ti)/StepNum
     ν = ki₀ = 1.
     ψ = 300
-    ζ = [10, 20, 40, 80]
+    ζ = [10, 22]
 
-    for η ∈ ζ
-        NN = MM = η
-        Ydata₁ = Array{Float64}(undef,ψ,StepNum+3,NN+2,MM+2)
-        κ₁ = Vector{Float64}(undef,ψ)
+    for α ∈ ["train" "valid" "test"]
 
-        for i ∈ 1:ψ
-            κ = rand(0.1:1e-5:0.4)
-            Ydata₁[i,:,:,:], κ₁[i] = Semicircle(NN,MM,κ,δ,ν,ki₀,tt,tf,Δₜ)
+        for η ∈ ζ
+            NN = MM = η
+            Ydata₁ = Array{Float64}(undef,ψ,StepNum+3,NN+2,MM+2)
+            κ₁ = Vector{Float64}(undef,ψ)
+
+            for i ∈ 1:ψ
+                κ = rand(0.1:1e-5:0.4)
+                Ydata₁[i,:,:,:], κ₁[i] = Semicircle(NN,MM,κ,δ,ν,ki₀,ti,tf,Δₜ)
+            end
+
+            PhaseFieldData = flatten!(PhaseFieldData, permutedims(Ydata₁,[3,4,2,1])) # Align the output .h5 for python 
+            κ = flatten!(κ,κ₁)
         end
 
-        PhaseFieldData = flatten!(PhaseFieldData, Ydata₁)
-        κ = flatten!(κ,κ₁)
+        # Training Dataset 
+        f = h5open("CE_$(α)_E1.h5","w")  #"r+"? / CE_train_$(Int(tf*60)).h5
+        create_group(f, "$α")
+        g = f["$α"]
+        for i ∈ 1:length(ζ)
+        g["pde_$(StepNum)-$(ζ[i])"] = PhaseFieldData[i]
+        attributes(g["pde_$(StepNum)-$(ζ[i])"])["dt"] = Δₜ
+        attributes(g["pde_$(StepNum)-$(ζ[i])"])["dx"] = 1/ζ[i]
+        attributes(g["pde_$(StepNum)-$(ζ[i])"])["nt"] = StepNum
+        attributes(g["pde_$(StepNum)-$(ζ[i])"])["nx"] = ζ[i]
+        attributes(g["pde_$(StepNum)-$(ζ[i])"])["tmax"] = tf
+        attributes(g["pde_$(StepNum)-$(ζ[i])"])["tmin"] = ti
+        attributes(g["pde_$(StepNum)-$(ζ[i])"])["x"] = Vector(range(0,ζ[i])) 
+        end
+        close(f)
+
     end
 
-    ω = OrderedDict(
-        "PhaseFieldData_$(ζ[1])"=>PhaseFieldData[1],
-        "PhaseFieldData_$(ζ[2])"=>PhaseFieldData[2],
-        "PhaseFieldData_$(ζ[3])"=>PhaseFieldData[3],
-        "PhaseFieldData_$(ζ[4])"=>PhaseFieldData[4],
-        )
-    save("CE_train_$(Int(tf*60)).h5", ω)
+    # ω = OrderedDict(
+    #     "PhaseFieldData_$(ζ[1])"=>PhaseFieldData[1],
+    #     "PhaseFieldData_$(ζ[2])"=>PhaseFieldData[2],
+    #     "PhaseFieldData_$(ζ[3])"=>PhaseFieldData[3],
+    #     "PhaseFieldData_$(ζ[4])"=>PhaseFieldData[4],
+    #     )
+    # save("CE_train_$(Int(tf*60)).h5", ω)
 end
 
 Phase = Data()
